b4b8de953e8809689cf309e620325058b91ec9f8
Rodrigo <rodrigo.fcardoso@hotmail.com>
2020-09-03 22:29:18 -0300

    First commit

---
+++ src/native-test/java/br/com/rodrigocardoso/repy/NativeClienteResourceIT.java
@@ -0,0 +1,9 @@
+package br.com.rodrigocardoso.repy;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeClienteResourceIT extends ClienteResourceTest {
+
+    // Execute the same tests but in native mode.
+}
